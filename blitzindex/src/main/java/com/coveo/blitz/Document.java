0788471bec58ea250f70a96205097080da5508b7
Alexandre Gariépy <gariepy.alex@gmail.com>
2014-01-11 12:45:07 -0500

    Adding document class

---
+++ blitzindex/src/main/java/com/coveo/blitz/Document.java
@@ -0,0 +1,25 @@
+package com.coveo.blitz;
+
+import java.util.List;
+
+public class Document {
+	private int id;
+	private List<String> tokens;
+	
+	public Document(int id, List<String> tokens) {
+		this.id = id;
+		this.tokens = tokens;
+	}
+	
+	public boolean match(String s){
+		String searchString = s.toLowerCase();
+		for(String t : tokens){
+			if(t.equals(searchString))
+				return true;
+		}
+		return false;
+	}
+	public int getId() {
+		return this.id;
+	}
+}
