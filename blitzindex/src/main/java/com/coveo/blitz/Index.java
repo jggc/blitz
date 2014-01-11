1b13719d37689c97d00884619af500bdea24a03b
Alexandre Gariépy <gariepy.alex@gmail.com>
2014-01-11 11:44:59 -0500

    Adding Index class

---
+++ blitzindex/src/main/java/com/coveo/blitz/Index.java
@@ -0,0 +1,24 @@
+package com.coveo.blitz;
+
+import java.util.List;
+
+import org.apache.thrift.TException;
+
+import com.coveo.blitz.thrift.Album;
+import com.coveo.blitz.thrift.Artist;
+
+public class Index {
+	
+	public void indexArtist(Artist artistToIndex) {
+		
+	}
+	
+	public void indexAlbum(Album albumToIndex) {
+		
+	}
+	
+	public List<Artist> searchArtist(){
+		return null;
+		
+	}
+}
