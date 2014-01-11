12b2d7dfb1fdf46e98625f336ebef0a20ba4331a
jeangab <jeangabriel.gc@gmail.com>
2014-01-11 15:11:03 -0500

    interface avance

--- ArchSide/WebContent/js/main.js
+++ ArchSide/WebContent/js/main.js
@@ -13,3 +13,41 @@ function request(value){
 				}
 			});
 }
+
+
+Artiste = {
+	"name" : "Nom",
+	"origin" : "Origine",
+	"active_start" : "Date de début",
+	"active_end" : "Date de fin",
+	"genres" : "Styles musicaux",
+	"labels" : "Labels",
+	"albums" : "Albums",
+	"group_names" : "Noms de groupes",
+	"intruments_played" : "Instruments joués"
+}; 
+Albums = {
+	"name" : "Nom",
+	"artists" : "Artistes",
+	"release_date" : "Date de sortie",
+	"genres" : "Styles musicaux",
+	"track_names" : "Noms des pistes"
+};	
+
+function createTiles( array){
+	
+	for(var key in array){
+		if( array.hasOwnProperty(key)){
+			
+			$cat = $(".templates .floatingTile").clone();
+			$cat.find("h3").text(array[key]);
+			
+			$cat.appendTo(".categories");
+		}
+	}
+}
+
+$(function(){
+	createTiles(Artiste);
+	createTiles(Albums);
+});
