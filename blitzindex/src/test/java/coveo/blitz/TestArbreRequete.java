a653cb4535f75832cb43089c38fc6ff279b740ef
David Landry <davidlandry93@gmail.com>
2014-01-11 12:03:07 -0500

    im in deep shit

---
+++ blitzindex/src/test/java/coveo/blitz/TestArbreRequete.java
@@ -0,0 +1,24 @@
+package coveo.blitz;
+
+import org.junit.Test;
+
+import com.coveo.blitz.ArbreRequete;
+import com.coveo.blitz.thrift.Query;
+import com.coveo.blitz.thrift.QueryTreeNode;
+import com.coveo.blitz.thrift.NodeType;
+
+public class TestArbreRequete {
+	
+	@Test
+	public void test()
+	{
+		Query requete = new Query();
+		requete.setRootId(1);
+		requete.queryTreeNodes.add(new QueryTreeNode("AND", NodeType.OPERATOR , 2,3,1));
+		requete.queryTreeNodes.add(new QueryTreeNode("pink", NodeType.LITERAL, -1,-1,2));
+		requete.queryTreeNodes.add(new QueryTreeNode("floyd", NodeType.LITERAL, -1,-1,3));
+		
+		ArbreRequete arbre = new ArbreRequete(requete);
+	}
+
+}
